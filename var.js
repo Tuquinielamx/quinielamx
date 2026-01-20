0a2bfacfa7b51e2231ea31eceef4901ce2b58cb3
Tuquinielamx <146607238+Tuquinielamx@users.noreply.github.com>
2026-01-20 09:52:24 -0600

    Update var.js
    actualizacion de numero de contato whatsapp

--- var.js
+++ var.js
@@ -1,6 +1,7 @@
-var tel  = "5213324124272";
-var telm = "5213324124272";
+var tel  = "5213330736572";
+var telm = "5213330736572";
 var suspIndex  = [];
 var suspIndexm = null;
 var price  = 25;
+
 var pricem = 20;
