e3594b21afebcff59656cdf3f3012f02a4de9ba9
Tuquinielamx <146607238+Tuquinielamx@users.noreply.github.com>
2026-01-20 19:57:52 -0600

    Update var.js
    Actualizacion de numero

--- var.js
+++ var.js
@@ -1,7 +1,8 @@
-var tel  = "5213330736572";
-var telm = "5213330736572";
+var tel  = "5213324124272";
+var telm = "5213324124272";
 var suspIndex  = [];
 var suspIndexm = null;
 var price  = 25;
 
 var pricem = 20;
+
