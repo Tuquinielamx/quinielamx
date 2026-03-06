76f5b90695b7fb037ae608a9ec949f50116c60c4
Tuquinielamx <146607238+Tuquinielamx@users.noreply.github.com>
2026-03-06 07:44:35 -0600

    Update var.js

--- var.js
+++ var.js
@@ -1,9 +1,10 @@
-var tel  = "5213352090502";
-var telm = "5213352090502";
+var tel  = "5213330736572";
+var telm = "5213330736572";
 var suspIndex  = [];
 var suspIndexm = null;
 var price  = 25;
 
 var pricem = 20;
+
 
 
